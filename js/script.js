34ac39b99f49641209bd1647561d82f98b89db00
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-04-04 18:40:40 +0000

    done?

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Julianne Leblanc-Peltier All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const radiusOfSphere = parseFloat(document.getElementById("radius-of-sphere").value)
+
+  // process
+  const volumeOfSphere = 4 / 3 * (Math.PI * radiusOfSphere**3)
+
+  // output
+  document.getElementById("volume-of-sphere").innerHTML = volumeOfSphere.toFixed(2) + " mm³"
+}
